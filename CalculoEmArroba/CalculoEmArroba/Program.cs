903ee03a96975c7a471a57e6783dda2f18781bb5
Cleo Celestino <cleocelestinoo@gmail.com>
2020-02-25 19:55:19 -0300

    Primeiro Commit

---
+++ CalculoEmArroba/CalculoEmArroba/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculoEmArroba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o peso em KG a ser convertido em arroba: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            int valorConvertido = valor / 30;
+
+            Console.WriteLine("O valor convertido em arroba é:" + valorConvertido);
+        }
+    }
+}
